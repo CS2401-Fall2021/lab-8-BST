8bd502139354757812cd72f913f4ff68380d984f
m-steph <86425576+m-steph@users.noreply.github.com>
2021-10-22 10:46:20 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+  }
+  
+  // Part 1: Calculate number of mistakes in a tree that starts at root. 
+  public static int numTreeMistakes(Node root){
+  }
+  
+  // Part 2: Calculate the maximum depth of the tree
+  public static int treeDepth(Node root){
+  }
+  // Part 3: Calculate the number of unique values in the tree
+  public static int treeItems(Node root){
+  }
+  // Part 4: Quality control! Combine parts 1-3 to see if we can keep the tree
+  public static boolean keepTree(Node root, int maxMistakes, double logFactor){
+  }
+}
