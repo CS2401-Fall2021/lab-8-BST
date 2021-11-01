9478d947e9a11e17fee3029888576df742b339d5
m-steph <86425576+m-steph@users.noreply.github.com>
2021-11-01 10:52:03 -0600

    Update Node.java

--- Node.java
+++ Node.java
@@ -5,5 +5,16 @@ public class Node {
   public  Node(char data){
     this.data = data;
   }
+    
+  public void print() { print(""); }
+  private void print(String prefix) {
+    System.out.println(prefix + this.data);
+    prefix = prefix.replace('\u251C', '\u2502');
+    prefix = prefix.replace('\u2514', ' ');
+    if(left != null) left.print(prefix + "\u251C ");
+    else if(right != null) System.out.println(prefix + "\u251C " + "\u2400");
+    if(right != null) right.print(prefix + "\u2514 ");
+    else if(left != null) System.out.println(prefix + "\u2514 " + "\u2400");
+  }
  
 }
